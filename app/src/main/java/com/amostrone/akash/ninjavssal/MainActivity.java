20cff72831a308b1c02a421bb744d73c63cfb758
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-28 09:29:06 +0530

    Added full screen mode.

--- app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         game =new Game(this);
         game.setBackgroundColor(Color.BLACK);
         setContentView(game);
     }
+
 }
