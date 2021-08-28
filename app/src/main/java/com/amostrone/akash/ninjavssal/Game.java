bdbd045f55172a9bfd08137cbc093a638162a31f
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-28 09:19:19 +0530

    Added canvas and set orientation to landscape mode.

---
+++ app/src/main/java/com/amostrone/akash/ninjavssal/Game.java
@@ -0,0 +1,10 @@
+package com.amostrone.akash.ninjavssal;
+
+import android.content.Context;
+import android.view.View;
+
+public class Game extends View {
+    public Game(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
@@ -2,13 +2,20 @@ package com.amostrone.akash.ninjavssal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Game game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game =new Game(this);
+        game.setBackgroundColor(Color.BLACK);
+        setContentView(game);
     }
 }
