7ed0a678eb242227ff7a980155ff4cd2627a19e5
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-28 09:39:09 +0530

    Added background image.

--- app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/ninjavssal/MainActivity.java
@@ -15,17 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View decorView = getWindow().getDecorView();
         // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
+        View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
         game =new Game(this);
-        game.setBackgroundColor(Color.BLACK);
+        game.setBackgroundResource(R.drawable.background);
         setContentView(game);
     }
 
